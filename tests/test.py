3106686a6e9d88de3cf7cebb8ec90e85856d9cbc
Diego Alejandro Irreño Torres <dirreno@unal.edu.co>
2025-04-09 21:39:59 -0500

    check colombia original error

--- tests/test.py
+++ tests/test.py
@@ -4,10 +4,10 @@ from socio4health import Extractor
 from socio4health.enums.data_info_enum import BraColnamesEnum, BraColspecsEnum
 from socio4health.harmonizer import Harmonizer, vertical_merge, drop_nan_columns
 
-col_extractor = Extractor(path="../../input/GEIH_2022/Original",down_ext=['.CSV','.csv','.zip'],sep=';')
-per_extractor = Extractor(path="../../input/ENAHO_2022/Original",down_ext=['.csv','.zip'])
-rd_extractor = Extractor(path="../../input/ENHOGAR_2022/Original",down_ext=['.csv','.zip'])
-bra_extractor = Extractor(path="../../input/PNADC_2022/Test",down_ext=['.txt','.zip'],is_fwf=True,colnames=BraColnamesEnum.PNADC.value, colspecs=BraColspecsEnum.PNADC.value)
+col_extractor = Extractor(path="../../input/GEIH_2022/Original",down_ext=['.CSV','.csv','.zip'],sep=';', download_dir="data")
+per_extractor = Extractor(path="../../input/ENAHO_2022/Original",down_ext=['.csv','.zip'], download_dir="data")
+rd_extractor = Extractor(path="../../input/ENHOGAR_2022/Original",down_ext=['.csv','.zip'], download_dir="data")
+bra_extractor = Extractor(path="../../input/PNADC_2022/Test",down_ext=['.txt','.zip'],is_fwf=True,colnames=BraColnamesEnum.PNADC.value, colspecs=BraColspecsEnum.PNADC.value, download_dir="data")
 
 def test(extractor):
     dfs = extractor.extract()
@@ -20,7 +20,7 @@ def test(extractor):
         with ProgressBar():
             print(df.head())
         # df.to_csv("data/output", index=False, single_file=True)
-        extractor.delete_download_folder()
+    extractor.delete_download_folder()
 
 if __name__ == "__main__":
-    test(bra_extractor)
+    test(col_extractor)

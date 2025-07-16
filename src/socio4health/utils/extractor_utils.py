5535f84f7d7773cb8212e43829d8a4da0000c259
Ingrid Vanessa Mora Sanchez <91691844+ingridvmoras@users.noreply.github.com>
2025-07-16 11:55:01 -0500

    Update extractor_utils.py

--- src/socio4health/utils/extractor_utils.py
+++ src/socio4health/utils/extractor_utils.py
@@ -17,7 +17,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 
 def run_standard_spider(url, depth, down_ext, key_words):
-    """Run the Scrapy spider to extract data from the given URL.
+    """Run the Scrapy spider to extract data from the given ``URL``.
 
     Parameters
     ----------
